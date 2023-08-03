1cdd76e045b16cbfc6dbdd797210db04f762d3ac
Sebastián Ayala Suárez <sebastianutpae@gmail.com>
2023-08-02 20:43:57 -0500

    Add: Supplier seeder and factory

---
+++ database/factories/SupplierFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Supplier>
+ */
+class SupplierFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'rut' => fake()->unique()->isbn10(),
+            'name' => fake()->company(),
+            'address_street' => fake()->streetName(),
+            'address_number' => fake()->numberBetween(10, 2000),
+            'address_commune' => fake()->streetName(),
+            'address_city' => fake()->city(),
+            'phone' => fake()->unique()->phoneNumber(),
+            'webpage' => fake()->domainName(),
+        ];
+    }
+}
---
+++ database/seeders/SuplierSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Supplier;
+
+class SuplierSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Supplier::factory()
+            ->count(100)
+            // TODO: Add products
+            ->create();
+    }
+}
