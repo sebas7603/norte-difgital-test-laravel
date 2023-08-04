69cdce7081c5d84ff58eb75f5fda68aa3d191c22
Sebastián Ayala Suárez <sebastianutpae@gmail.com>
2023-08-03 19:38:40 -0500

    Add: Query params to filter the results in Product index method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\JoinClause;
 
 class ProductController extends Controller
 {
@@ -13,9 +15,37 @@ class ProductController extends Controller
      */
     public function index(Request $request): JsonResponse
     {
-        $itemsPerPage = $request->query('items', 10);
-        $products = Product::withCount('sales')->with('supplier')->orderBy('name', 'asc')->paginate($itemsPerPage);
-        return response()->json($products, 200);
+        try {
+            $branchId = $request->query('branch', null);
+            $supplierId = $request->query('supplier', null);
+            $itemsPerPage = $request->query('items', 10);
+            $products = Product::withCount('sales')
+                ->when($branchId, function (Builder $query, int $branchId) {
+                    $query->leftJoin('branch_product', function (JoinClause $join) use ($branchId) {
+                        $join->on('branch_product.product_id', '=', 'products.id');
+                    })->where('branch_product.branch_id', '=', $branchId);
+                })
+                ->when($supplierId, function (Builder $query, int $supplierId) {
+                    $query->where('products.supplier_id', '=', $supplierId);
+                })
+                ->with('supplier')
+                ->with('branches')
+                ->orderBy('name', 'asc')
+                ->paginate($itemsPerPage);
+            return response()->json($products, 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Ups! There was a server error.',
+                'error' => [
+                    'class' => get_class($th),
+                    'message' => $th->getMessage(),
+                    'code' => $th->getCode(),
+                    'file' => $th->getFile(),
+                    'line' => $th->getLine(),
+                    'trace' => $th->getTrace()
+                ]
+            ], 500);
+        }
     }
 
     /**
